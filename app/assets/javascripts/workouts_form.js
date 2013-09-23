13c81dc71f7f11a7b831cbbcc9d3d41b26fde579
Luke Robertson <luke.robertson@mac.com>
2013-09-23 16:13:05 +0100

    initial jquery added to new workout form

---
+++ app/assets/javascripts/workouts_form.js
@@ -0,0 +1,121 @@
+var setTypeChange = function(selector){
+  selector.change(function() {
+
+
+  if(selector.val() === "Distance"){
+
+
+    if(selector.parent().parent().children('.resistance_fields').css('display') !== 'none'){
+      selector.parent().parent().children('.resistance_fields').animate({
+         height: "toggle",
+          opacity: 1
+      }, 150)
+    }
+
+    if(selector.parent().parent().children('.cardio_fields').css('display') === 'none'){
+      selector.parent().parent().children('.cardio_fields').animate({
+        height: "toggle",
+        opacity: 1
+      }, 150, function(){
+        selector.parent().parent().children('.cardio_fields').children('.distance').fadeIn(150)
+      })
+    }else{
+
+        selector.parent().parent().children('.cardio_fields').children('.interval').fadeOut(150, function(){
+              selector.parent().parent().children('.cardio_fields').children('.distance').fadeIn(150);
+            }
+          );
+    }
+   }
+
+
+   if(selector.val() === "Time Interval"){
+
+
+    if(selector.parent().parent().children('.resistance_fields').css('display') !== 'none'){
+      selector.parent().parent().children('.resistance_fields').animate({
+         height: "toggle",
+          opacity: 1
+      }, 150)
+    }
+
+    if(selector.parent().parent().children('.cardio_fields').css('display') === 'none'){
+      selector.parent().parent().children('.cardio_fields').animate({
+        height: "toggle",
+        opacity: 1
+      }, 150, function(){
+        selector.parent().parent().children('.cardio_fields').children('.interval').fadeIn(150)
+      })
+    }else{
+
+        selector.parent().parent().children('.cardio_fields').children('.distance').fadeOut(150, function(){
+          selector.parent().parent().children('.cardio_fields').children('.interval').fadeIn(150);
+        });
+
+    }
+   }
+
+  if(selector.val() === "Resistance"){
+
+
+    if(selector.parent().parent().children('.cardio_fields').css('display') !== 'none'){
+      selector.parent().parent().children('.cardio_fields').animate({
+        height: "toggle",
+        opacity: 1
+      }, 150, function(){
+        selector.parent().parent().children('.cardio_fields').children('.distance').css({display: 'none'});
+        selector.parent().parent().children('.cardio_fields').children('.interval').css({display: 'none'});
+      })
+    }
+
+    if(selector.parent().parent().children('.resistance_fields').css('display') === 'none'){
+      selector.parent().parent().children('.resistance_fields').animate({
+        height: "toggle",
+        opacity: 1
+      }, 150)
+    }
+
+   }
+
+  if(selector.val() === ""){
+
+
+    if(selector.parent().parent().children('.resistance_fields').css('display') !== 'none'){
+      selector.parent().parent().children('.resistance_fields').animate({
+        height: "toggle",
+        opacity: 1
+      }, 150, function(){
+
+      })
+    };
+
+     if(selector.parent().parent().children('.cardio_fields').css('display') !== 'none'){
+      selector.parent().parent().children('.cardio_fields').animate({
+        height: "toggle",
+        opacity: 1
+      }, 150)
+    }
+
+
+   };
+
+
+  })
+}
+
+
+
+
+$(function() {
+
+var initial_fields = $('.set_type_select');
+setTypeChange(initial_fields);
+
+
+$(document).on('nested:fieldAdded', function(event){
+  var selector = event.field.find('.set_type_select');
+  setTypeChange(selector);
+})
+
+
+});
