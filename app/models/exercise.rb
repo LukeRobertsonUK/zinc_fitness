ddcdae2b55b7a2803f9dbbcaffbb1ca54c876a22
Luke Robertson <luke.robertson@mac.com>
2013-10-15 11:54:07 +0100

    changes order of exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,7 @@ class Exercise < ActiveRecord::Base
   attr_accessible :description, :name
   has_many :exercise_sets
   has_many :workouts, through: :exercise_sets
-
+  default_scope order('name' )
   validates :name, presence: true
   validates :name, uniqueness: true
 
